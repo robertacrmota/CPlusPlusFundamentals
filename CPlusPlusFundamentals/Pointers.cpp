576524648aa232e026da4ebc73f77bad11b3b5b8
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2023-04-08 13:25:23 -0300

    Update Pointers tutorial

--- CPlusPlusFundamentals/Pointers.cpp
+++ CPlusPlusFundamentals/Pointers.cpp
@@ -11,9 +11,45 @@ using std::cin;
 	- What are pointers?
 	- Address-of operator (&)
 	- Dereference operator (*)
+	- Why use pointers?
+		- return multiple values from a function
 */
 
 namespace pointers {
+
+	int getMin(int numbers[], int size) {
+		int min = numbers[0];
+
+		for (int i = 1; i < size; i++) {
+			if (min > numbers[i])
+				min = numbers[i];
+		}
+
+		return min;
+	}
+
+	int getMax(int numbers[], int size) {
+		int max = numbers[0];
+
+		for (int i = 1; i < size; i++) {
+			if (max < numbers[i])
+				max = numbers[i];
+		}
+
+		return max;
+	}
+
+	void getMinAndMax(int numbers[], int size, int* min, int* max) {
+		*min = numbers[0];
+		*max = numbers[0];
+
+		for (int i = 1; i < size; i++) {
+			if (*min > numbers[i])
+				*min = numbers[i];
+			if (*max < numbers[i])
+				*max = numbers[i];
+		}
+	}
 
 	int main(int argc, char* argv[]) {
 		
@@ -41,7 +77,18 @@ namespace pointers {
 		*ptr2 = 15;
 		cout << *ptr2 << endl;
 
+		// Why use pointers?
 
+
+		// 1. return multiple values from a function
+		int numbers[5] = { 5, 4, -2, 26, 3 };
+		cout << "Min is " << getMin(numbers, 5) << endl;
+		cout << "Max is " << getMax(numbers, 5) << endl;
+
+		int min, max;
+		getMinAndMax(numbers, 5, &min, &max);
+		cout << "Min is " << min << endl;
+		cout << "Max is " << max << endl;
 
 		return 0;
 	}
--- CPlusPlusFundamentals/Pointers.h
+++ CPlusPlusFundamentals/Pointers.h
@@ -1,6 +1,10 @@
 #pragma once
 
 namespace pointers {
+	int getMin(int numbers[], int size);
+	int getMax(int numbers[], int size);
+	void getMinAndMax(int numbers[], int size, int* min, int* max);
+
 	int main(int argc, char* argv[]);
 }
 
